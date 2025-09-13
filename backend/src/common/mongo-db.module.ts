a4bf8b61f43f803d5426dbec71b811721848ceaa
Gaston Minetti <gastonminetti4@gmail.com>
2025-09-13 19:55:17 +0200

    refactor mongo module

---
+++ backend/src/common/mongo-db.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('MONGO_CONNECTION_STRING'),
+        dbName: 'trading_bot',
+      }),
+    }),
+  ],
+
+  exports: [MongooseModule],
+})
+export class MongoDbModule {}
