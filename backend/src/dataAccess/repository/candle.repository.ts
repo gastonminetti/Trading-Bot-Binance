0eb4b77d3b240124d642225d27b7add5af935f6b
Gaston Minetti <gastonminetti4@gmail.com>
2025-09-17 14:17:28 +0200

    removing unnecessary import from candle repo

--- backend/src/dataAccess/repository/candle.repository.ts
+++ backend/src/dataAccess/repository/candle.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, FilterQuery, Query } from 'mongoose';
+import { Model, FilterQuery } from 'mongoose';
 
 import { CandleMongo } from '../Schemas/candle/candle.schema';
 import { CandleRecord } from '../Schemas/candle/candle.record';
